e391b95aacdab448d84b16305fd39f245e71b901
Rahmat Sulistio <40190205+rahmat-st@users.noreply.github.com>
2020-08-20 11:25:21 +0700

    Add input change and submit functions with hooks

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoForm = () => {
+  const [value, setValue] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!value) {
+      alert("No blank todo!");
+      return;
+    }
+
+    alert(value);
+  };
+
   return (
     <section className="form-add-todo">
-      <form className="add-form">
-        <input type="text" className="add-input" />
+      <form className="add-form" onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          className="add-input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <button className="btn">Add</button>
       </form>
     </section>
